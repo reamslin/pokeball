0ca674cc25cac2a71ac146e66ce652ff537846ec
Lindsey Reams <reamslin@gmail.com>
2024-04-21 23:52:02 -0400

    l

--- src/script.js
+++ src/script.js
@@ -201,7 +201,7 @@ const tick = () => {
   // Update controls
   controls.update();
 
-  // update sphere /
+  // update sphere
   sphere.position.x = Math.cos(elapsedTime) * 1.5;
   sphere.position.z = Math.sin(elapsedTime) * 1.5;
   sphere.position.y = Math.abs(Math.sin(elapsedTime * 3));
